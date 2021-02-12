b4659234e48cc9f93d4ef1daa702c69a501c23b7
giovdellap <giovanninicola.dellapelle@mail.polimi.it>
2021-02-13 00:55:30 +0100

    debug login

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -2,10 +2,12 @@ import { handleResponse, handleError } from "./apiUtils";
 const baseUrl = "http://localhost/Immobile17/api/Utente";
 
 export function sendLogin(email, password) {
+	console.log("sendlogin");
 	const formData = new FormData();
 	formData.append("email", email);
 	formData.append("password", password);
-	return fetch(baseUrl + "/Login", {
+
+	return fetch(baseUrl + "/login", {
 		method: "POST",
 		body: formData,
 	})
